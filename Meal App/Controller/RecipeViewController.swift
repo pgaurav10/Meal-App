5c791deb5d6a49159329f38bdf182d2f3865a3f6
Gaurav <gauravpatil24121997@gmail.com>
2021-11-16 20:04:33 -0600

    Minor Fixes on last page

--- Meal App/Controller/RecipeViewController.swift
+++ Meal App/Controller/RecipeViewController.swift
@@ -32,7 +32,7 @@ class RecipeViewController: UIViewController {
         
         self.video.text = "Video: "
         self.recipe_label.text = "Recipe: "
-        print(self.id_text)
+        //print(self.id_text)
         loadRecipeDetail()
         
     }
@@ -50,83 +50,83 @@ class RecipeViewController: UIViewController {
         self.link.text = recipe?.youtube
         
         
-        if(recipe?.inr1 != "") {
+        if(recipe?.inr1 != "" && recipe?.msr1 != "") {
             str = "Ingredients: \((recipe?.inr1)!)\nQuantity: \((recipe?.msr1)!)\n\n"
         }
         
-        if(recipe?.inr2 != "") {
+        if(recipe?.inr2 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr2)!)\nQuantity: \((recipe?.msr2)!)\n\n")
         }
         
-        if(recipe?.inr3 != "") {
+        if(recipe?.inr3 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr3)!)\nQuantity: \((recipe?.msr3)!)\n\n")
         }
         
-        if(recipe?.inr4 != "") {
+        if(recipe?.inr4 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr4)!)\nQuantity: \((recipe?.msr4)!)\n\n")
         }
         
-        if(recipe?.inr5 != "") {
+        if(recipe?.inr5 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr5)!)\nQuantity: \((recipe?.msr5)!)\n\n")
         }
         
-        if(recipe?.inr6 != "") {
+        if(recipe?.inr6 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr6)!)\nQuantity: \((recipe?.msr6)!)\n\n")
         }
         
-        if(recipe?.inr7 != "") {
+        if(recipe?.inr7 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr7)!)\nQuantity: \((recipe?.msr7)!)\n\n")
         }
         
-        if(recipe?.inr8 != "") {
+        if(recipe?.inr8 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr8)!)\nQuantity: \((recipe?.msr8)!)\n\n")
         }
         
-        if(recipe?.inr9 != "") {
+        if(recipe?.inr9 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr9)!)\nQuantity: \((recipe?.msr9)!)\n\n")
         }
         
-        if(recipe?.inr10 != "") {
+        if(recipe?.inr10 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr10)!)\nQuantity: \((recipe?.msr10)!)\n\n")
         }
         
-        if(recipe?.inr11 != "") {
+        if(recipe?.inr11 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr11)!)\nQuantity: \((recipe?.msr11)!)\n\n")
         }
         
-        //if(recipe?.inr12 != "") {
-          //  str?.append( "Ingredients: \((recipe?.inr12)!)\t Measure: \((recipe?.msr12)!)\n\n")
-        //}
+        if(recipe?.inr12 != "" && recipe?.msr1 != "") {
+            str?.append( "Ingredients: \((recipe?.inr12)!)\t Measure: \((recipe?.msr12)!)\n\n")
+        }
         
-        if(recipe?.inr13 != "") {
+        if(recipe?.inr13 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr13)!)\nQuantity: \((recipe?.msr13)!)\n\n")
         }
         
-        if(recipe?.inr14 != "") {
+        if(recipe?.inr14 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr14)!)\nQuantity: \((recipe?.msr14)!)\n\n")
         }
         
-        if(recipe?.inr15 != "") {
+        if(recipe?.inr15 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr15)!)\nQuantity: \((recipe?.msr15)!)\n\n")
         }
         
-        if(recipe?.inr16 != "") {
+        if(recipe?.inr16 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr16)!)\nQuantity: \((recipe?.msr16)!)\n\n")
         }
         
-        if(recipe?.inr17 != "") {
+        if(recipe?.inr17 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr17)!)\nQuantity: \((recipe?.msr17)!)\n\n")
         }
         
-        if(recipe?.inr18 != "") {
+        if(recipe?.inr18 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr18)!)\nQuantity: \((recipe?.msr18)!)\n\n")
         }
         
-        if(recipe?.inr19 != "") {
+        if(recipe?.inr19 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr19)!)\nQuantity: \((recipe?.msr19)!)\n\n")
         }
         
-        if(recipe?.inr20 != "") {
+        if(recipe?.inr20 != "" && recipe?.msr1 != "") {
             str?.append( "Ingredients: \((recipe?.inr20)!)\nQuantity: \((recipe?.msr20)!)\n\n")
         }
         
